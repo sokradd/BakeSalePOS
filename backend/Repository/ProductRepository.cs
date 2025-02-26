79e4c4dcdc01960a09b120cf7ea499ed48faf02e
sokradd <ssokradd@gmail.com>
2025-02-26 16:12:40 +0200

    setup methods for Repository

--- backend/Repository/ProductRepository.cs
+++ backend/Repository/ProductRepository.cs
@@ -1,6 +1,36 @@
-namespace BakeSale.API.Repository;
+using BakeSale.API.Data;
+using BakeSale.API.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class ProductRepository
+namespace BakeSale.API.Repositories;
+
+public class ProductRepository : IProductRepository
 {
-    
+    private readonly BakeSaleContext _context;
+
+    public ProductRepository(BakeSaleContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllProductsAsync()
+    {
+        return await _context.Products.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Product?> GetProductByIdAsync(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+
+    public async Task UpdateProductAsync(Product product)
+    {
+        _context.Entry(product).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> ProductExistsAsync(int id)
+    {
+        return await _context.Products.AnyAsync(p => p.Id == id);
+    }
 }
