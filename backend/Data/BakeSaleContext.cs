7cb886ebe67272f6a070177deeb488d3c5b5c3c9
sokradd <ssokradd@gmail.com>
2025-03-01 02:22:37 +0200

    changed and improved logic for ModelBuilder

--- backend/Data/BakeSaleContext.cs
+++ backend/Data/BakeSaleContext.cs
@@ -5,10 +5,13 @@ namespace BakeSale.API.Data;
 
 public class BakeSaleContext : DbContext
 {
-    public BakeSaleContext(DbContextOptions<BakeSaleContext> options) : base(options) { }
+    public BakeSaleContext(DbContextOptions<BakeSaleContext> options) : base(options)
+    {
+    }
 
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderLine> OrderLines { get; set; }
+    public DbSet<Payment> Payments { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Salesperson> Salespersons { get; set; }
 
@@ -16,19 +19,19 @@ public class BakeSaleContext : DbContext
     {
         modelBuilder.Entity<OrderLine>()
             .HasOne(ol => ol.Order)
-            .WithMany(o => o.OrderLines) 
+            .WithMany(o => o.OrderLines)
             .HasForeignKey(ol => ol.OrderId)
             .OnDelete(DeleteBehavior.Cascade);
 
         modelBuilder.Entity<OrderLine>()
             .HasOne(ol => ol.Product)
-            .WithMany(p => p.OrderLines)  
+            .WithMany(p => p.OrderLines)
             .HasForeignKey(ol => ol.ProductId)
             .OnDelete(DeleteBehavior.Cascade);
 
         modelBuilder.Entity<Order>()
             .HasOne(o => o.Salesperson)
-            .WithMany(s => s.Orders) 
+            .WithMany(s => s.Orders)
             .HasForeignKey(o => o.SalespersonId)
             .OnDelete(DeleteBehavior.Cascade);
     }
