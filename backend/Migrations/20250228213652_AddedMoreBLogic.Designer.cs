0c4aae5401159f72315730a66f3bc9ea80b4f011
sokradd <ssokradd@gmail.com>
2025-03-01 16:19:21 +0200

    initial commit

--- backend/Migrations/20250228213652_AddedMoreBLogic.Designer.cs
+++ backend/Migrations/20250228213652_AddedMoreBLogic.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BakeSale.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BakeSale.API.Migrations
 {
     [DbContext(typeof(BakeSaleContext))]
-    partial class BakeSaleContextModelSnapshot : ModelSnapshot
+    [Migration("20250228213652_AddedMoreBLogic")]
+    partial class AddedMoreBLogic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -37,6 +40,7 @@ namespace BakeSale.API.Migrations
                         .HasColumnType("integer");
 
                     b.Property<string>("Status")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<decimal>("TotalAmount")
